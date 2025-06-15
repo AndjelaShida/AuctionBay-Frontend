4cb9df5b800835d508c585432796edc6bf4457cc
Andjela Shida <andjelasheeda@gmail.com>
2025-06-15 15:45:52 +0200

    initializing project, config router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import AppRouter from './routes/AppRouter.tsx'
+import React from 'react'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ <React.StrictMode>
+  <AppRouter />
+  </React.StrictMode>
 )
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,2 +1,20 @@
 //fajl se konfiguraciju rutera. Router je deo aplikacija koji kontrolise navigaciju izmedju razlicitih stranica
 //ili pogleda unutar aplikacije.
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const AppRouter: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auctions" element={<Auctions />} />
+        <Route path="/auctions/:id" element={<AuctionDetails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter ;
